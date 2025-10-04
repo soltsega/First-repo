938b2f02f1ed931dcf351baec616eb5236b26790
Solomon <tsegasolomon538@gmail.com>
2025-10-04 17:47:07 +0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,14 @@ import './index.css'
 import App from './App.jsx'
 
 
-
+function Output() {
+  return (
+    <h1>This is my first repo</h1>
+  )
+}
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+
   </StrictMode>,
 )
