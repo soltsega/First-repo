4d0f624578f9ab89616e17ff802eec0b8136e8fa
Solomon <tsegasolomon538@gmail.com>
2025-10-05 12:26:15 +0300

    'I added profile instances'

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,107 @@ function App() {
 
   return (
     <>
-      
+      {/* The first person */}
+      <div className='contacts'>
+        <article className='contact-card'>
+          <img src=''
+            alt=''
+          />
+          <h3>Mr. Whiskerson</h3>
+
+          <div className='info-group'>
+            <img
+              src=''
+              alt=''
+            />
+            <p>+251938235449</p>
+          </div>
+
+          <div className='info-group'>
+            <img
+              src=''
+              alt=''
+            />
+            <p>whisker@gmail.com</p>
+          </div>
+        </article>
+      </div>
+
+      {/* The socond person */}
+      <div className='contacts'>
+        <article className='contact-card'>
+          <img src=''
+            alt=''
+          />
+          <h3>Mr. Fluffykins</h3>
+
+          <div className='info-group'>
+            <img
+              src=''
+              alt=''
+            />
+            <p>+251938235449</p>
+          </div>
+
+          <div className='info-group'>
+            <img
+              src=''
+              alt=''
+            />
+            <p>fluff@gmail.com</p>
+          </div>
+        </article>
+      </div>
+
+      <div className='contacts'>
+        <article className='contact-card'>
+          <img src=''
+            alt=''
+          />
+          <h3>Mr. Felix</h3>
+
+          <div className='info-group'>
+            <img
+              src=''
+              alt=''
+            />
+            <p>+251938235449</p>
+          </div>
+
+          <div className='info-group'>
+            <img
+              src=''
+              alt=''
+            />
+            <p>felix@gmail.com</p>
+          </div>
+        </article>
+      </div>
+
+      <div className='contacts'>
+        <article className='contact-card'>
+          <img src=''
+            alt=''
+          />
+          <h3>Mr. Pumpkin</h3>
+
+          <div className='info-group'>
+            <img
+              src=''
+              alt=''
+            />
+            <p>+251938235449</p>
+          </div>
+
+          <div className='info-group'>
+            <img
+              src=''
+              alt=''
+            />
+            <p>pumpkin@gmail.com</p>
+          </div>
+        </article>
+      </div>
     </>
   )
 }
