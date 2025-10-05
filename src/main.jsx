f8937af9f7f905735386650be2fa20f10833d9ed
Solomon <tsegasolomon538@gmail.com>
2025-10-05 11:40:27 +0300

    "Added name features"

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,13 @@ import App from './App.jsx'
 
 
 function Output() {
+  const firstName = "Solomon";
+  const lastName = "Tsega";
   return (
-    <h1>This is my first repo</h1>
+    <>
+      <h1>This is my first repo</h1>
+      <p>My name is {firstName} {lastName}.</p>
+    </>
   )
 }
 createRoot(document.getElementById('root')).render(
