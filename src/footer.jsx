e793332f65a72d7268ced350b4e5fcd631c2b42d
Solomon <tsegasolomon538@gmail.com>
2025-10-05 16:37:10 +0300

    Added footer.jsx

---
+++ src/footer.jsx
@@ -0,0 +1,8 @@
+import "./footer.css"
+export default function Footer() {
+    return (
+        <footer>
+            <p>© 2024 My First React Repo. All rights reserved.</p>
+        </footer>
+    )
+}
